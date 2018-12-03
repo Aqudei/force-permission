643cc4ca088760f074a8f9082ddb1034ed7a69fc
Aqudei <fdeu.aalmine@gmail.com>
2018-12-03 01:56:03 -0500

    fixed some problems

--- forcePermissions.py
+++ forcePermissions.py
@@ -2,14 +2,18 @@ import os
 import yaml
 import pwd
 import grp
+import shutil
 
 START_DIR = './sample'
 
 PERMISSION_FILENAME = '.filePermissions'
 
+cached_permission_file = dict()
 
 # searching for permission file is done by traversing directory upward
 # looking for .filePermissions file
+
+
 def search_for_filepermission(folder):
 
     files = os.listdir(folder)
@@ -21,10 +25,39 @@ def search_for_filepermission(folder):
         return search_for_filepermission(head)
 
 
+def read_and_cache_permission_file(permission_file):
+    print('Caching permission file: {}'.format(permission_file))
+    with open(permission_file, 'rt') as fp:
+
+        filepermission_info = yaml.load(fp)
+
+        cached_permission_file[permission_file] = dict()
+
+        if 'user' in filepermission_info:
+            cached_permission_file[permission_file]['user'] = filepermission_info['user']
+        else:
+            cached_permission_file[permission_file]['user'] = None
+
+        if 'group' in filepermission_info:
+            cached_permission_file[permission_file]['group'] = filepermission_info['group']
+        else:
+            cached_permission_file[permission_file]['group'] = None
+
+        if 'chmod' in filepermission_info:
+            cached_permission_file[permission_file]['chmod'] = int(
+                filepermission_info['chmod'], 8)
+
+
+def get_file_info(filename):
+    stats = os.stat(filename)
+    return (stats.st_mode & 0o777, pwd.getpwuid(stats.st_uid).pw_name, grp.getgrgid(stats.st_gid).gr_name)
+
+
 if __name__ == "__main__":
 
     # Root directory MUST have .forcePermission file
     if not PERMISSION_FILENAME in os.listdir(START_DIR):
+        print('Root .filePermission file must exist.')
         raise FileNotFoundError()
 
     # Loop through all files in the directory starting at the deepest level (BOTTOM UP)
@@ -32,23 +65,47 @@ if __name__ == "__main__":
 
         # See comments for search_for_filepermission()
         permission_file = search_for_filepermission(root)
+        if permission_file in cached_permission_file:
+            print('Using cached version of {}'.format(permission_file))
+        else:
+            read_and_cache_permission_file(permission_file)
 
-        with open(permission_file, 'rt') as fp:
-            filepermission_info = yaml.load(fp)
-            latest_user_value = filepermission_info['user']
-            latest_group_value = filepermission_info['group']
+        filepermission_info = cached_permission_file[permission_file]
+
+        using_user = filepermission_info['user']
+        using_group = filepermission_info['group']
+
+        using_chmod = filepermission_info.get('chmod')
 
         print('Processing files inside folder : \'{}\''.format(root))
-        for f in files:
-            if f == PERMISSION_FILENAME:
-                # We ignore permission file
-                continue
 
+        for f in files:
             filename = os.path.join(root, f)
-            try:
-                uid = pwd.getpwnam(latest_user_value).pw_uid
-                gid = grp.getgrnam(latest_group_value).gr_gid
-                os.chown(filename, uid, gid)
-            except KeyError as ex:
+
+            mod, uname, gname = get_file_info(filename)
+
+            if not using_group and not using_user:
                 print(
-                    'User: {} or Group: {} not found'.format(latest_user_value, latest_group_value))
+                    "file {} was skipped in chown because the yaml contains invalid user/group".format(filename))
+            else:
+
+                if uname == using_user and gname == using_group:
+                    print(
+                        "file {} was skipped in chown because file already has correct user/group".format(filename))
+                else:
+                    try:
+                        print("Using chown on {} with user: {} and group: {}".format(
+                            filename, using_user, using_group))
+                        shutil.chown(filename, user=using_user,
+                                     group=using_group)
+                        print(
+                            'Success: Chown was executed on file: {}'.format(filename))
+                    except Exception as e:
+                        print(e)
+
+            if using_chmod:
+                if not mod == using_chmod:
+                    os.chmod(filename, using_chmod)
+
+    for d in dirs:
+        directory = os.path.join(root, d)
